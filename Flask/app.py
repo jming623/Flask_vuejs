686e79b1ffd408a06b6ed6953bcdbd866c2e45b8
jming623 <jming623@naver.com>
2022-09-18 02:01:41 +0900

    1일차 Flask와 VueJS연결 완료

--- Flask/app.py
+++ Flask/app.py
@@ -1,18 +1,26 @@
-from flask import Flask
+import imp
+import json
+from flask import Flask, jsonify
+from flask_cors import CORS
  
 app = Flask(__name__)
- 
- 
+CORS(app) #Cross-Origin Resource Sharing) 모든 요청에 대해 cors를 적용한다는 의미
+#동일 출처 정책(Same-origin policy)에 따라,
+#다른 호스트(ip및 포트가 다른)에서 리소스에 접근할 수 없도록 함.
+#CORS는 교차 출처 리소스 공유(cors)를 처리하기 위한 패키지
+
+#cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+#/api/로 들어오는 모든 요청에 대해서만 cors를 적용한다는 의미
+
 @app.route('/')
 def index():
     return 'Hello World'
- 
-@app.route('/create/')
-def create():
-    return 'create'
 
-@app.route('/read/<bno>/')
-def read(bno):
-    return 'Read ' + bno    
+@app.route('/api/board/')
+#@cross_origin() #해당 요청에 대해서만 cors를 적용한다는 의미
+def getBoardList():
+    print('넘어옴')
+    data = {"result_code": "success", "result_data": "통신성공"}
+    return jsonify(data)    
 
 app.run(debug=True)
